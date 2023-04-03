af0b3c7c911a61224e0699c10878ba0c4c4dbcfb
Facundo Aletto <facundoaletto95@gmail.com>
2023-04-03 20:01:53 -0300

    Segunda Parte

--- algoritmo1-6.js
+++ algoritmo1-6.js
@@ -1,2 +1,14 @@
-let NombreIngresado = prompt("Nombre de usuario");
-document.write("<h1> Hola"+ " " + NombreIngresado + "</h1>");
+let Nombre = prompt("Nombre y Aperllido");
+document.write("<h1> Hola"+ " " + Nombre + "</h1>");
+
+let NumeroDocumento = prompt("Numero de documento");
+document.write("<h2> Documento N°"+ " " + NumeroDocumento + "</h2>");
+
+let MayoriaEdad = prompt("¿Sos mayor de 18 años? Si soy, si es mayor. No soy, en caso contrario");
+document.write("<h2>" + MayoriaEdad+ " " + "acepto términos y condiciones</h2>");
+
+let Peso = prompt("Ingrese su peso con decimales");
+document.write("<h2> Su peso es"+ " " + Peso + "</h2>");
+
+let Email = prompt("Ingrese su email para recibir información sobre suplementos");
+document.write("<h2> Su email es:"+ " " + Email + "</h2>");
