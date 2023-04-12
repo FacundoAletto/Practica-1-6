0ecfc1c7f84554c7269b4d7f71bf3e6954e9f62f
Facundo Aletto <facundoaletto95@gmail.com>
2023-04-12 00:36:58 -0300

    Practica 1-6 1er parte resuelta

--- algoritmo1-6.js
+++ algoritmo1-6.js
@@ -1,14 +1,5 @@
-let Nombre = prompt("Nombre y Aperllido");
-document.write("<h1> Hola"+ " " + Nombre + "</h1>");
+let nombre = prompt("Escriba su nombre");
 
-let NumeroDocumento = prompt("Numero de documento");
-document.write("<h2> Documento N°"+ " " + NumeroDocumento + "</h2>");
-
-let MayoriaEdad = prompt("¿Sos mayor de 18 años? Si soy, si es mayor. No soy, en caso contrario");
-document.write("<h2>" + MayoriaEdad+ " " + "acepto términos y condiciones</h2>");
-
-let Peso = prompt("Ingrese su peso con decimales");
-document.write("<h2> Su peso es"+ " " + Peso + "</h2>");
-
-let Email = prompt("Ingrese su email para recibir información sobre suplementos");
-document.write("<h2> Su email es:"+ " " + Email + "</h2>");
+console.log (nombre);
+document.querySelector("body").innerHTML = "<h1> "+ nombre +" </h1>";
+document.write("<h2> Mi nombre es "+ nombre +" </h2>");
