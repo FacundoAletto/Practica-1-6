562aca9191d8f92c349fa4b3ce2308bc3ae0e2d5
Facundo Aletto <facundoaletto95@gmail.com>
2023-04-12 00:44:45 -0300

    Practica 1-6 2da parte resuelta

--- algoritmo1-6.js
+++ algoritmo1-6.js
@@ -1,5 +1,19 @@
-let nombre = prompt("Escriba su nombre");
+/* let nombre = prompt("Escriba su nombre");
 
 console.log (nombre);
 document.querySelector("body").innerHTML = "<h1> "+ nombre +" </h1>";
-document.write("<h2> Mi nombre es "+ nombre +" </h2>");
+document.write("<h2> Mi nombre es "+ nombre +" </h2>"); */
+
+let nombre = prompt("Nombre y Apellido");
+let numeroEntero = prompt("Ingrese un número entero");
+let booleano = prompt("Ingrese el tipo de dato booleano");
+let numeroDecimal = prompt("Ingrese un número decimal");
+let caracter = prompt("Ingrese un caracter");
+let palabra = prompt("Ingrese una palabra");
+
+document.write("<h1> Hola " + nombre + "</h1>");
+document.write("<h2> Número entero elegido: "+ numeroEntero + "</h2>");
+document.write("<h2> Dato booleano elegido: " + booleano + "</h2>");
+document.write("<h2> Número decimal elegido: " + numeroDecimal + "</h2>");
+document.write("<h2> Caracter ingresado: " + caracter + "</h2>");
+document.write("<h2> Palabra elegida: " + palabra + "</h2>");
